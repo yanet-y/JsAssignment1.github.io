fc4d540891319b6582e66b4847cef36f0e6c9997
yanet-y <107769377+yanet-y@users.noreply.github.com>
2024-09-30 10:45:13 +0300

    Add files via upload

---
+++ Exercises.js
@@ -0,0 +1,21 @@
+var a,b,c;
+
+function areaOfTriangle(a,b,c){
+    area = (a * b) / 2; //assuming a and b are the base and height of the triangle respectively
+    console.log("The area of the triangle is:", area)
+     
+}
+
+areaOfTriangle(5,6,7);
+
+var newString
+function addString(string) {
+    newString= "JavaScript " + string;
+    console.log(newString);
+    
+}
+  
+addString("is useful for building websites");
+  
+  
+  
